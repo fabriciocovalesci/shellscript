1ed550136ecd6f96bdf5a92780cf8b3e646d3e4c
Fabricio <fabcovalesci@gmail.com>
2020-05-12 16:50:41 -0300

    script que busca hora, pid, tamanho

--- Conversao-jpg-png/verifica_processos.sh
+++ Conversao-jpg-png/verifica_processos.sh
@@ -3,5 +3,6 @@
 processos=$(ps -e -o pid --sort -size | head -n 11)
 for pid in $processos
 do
-    echo $(ps -p $pid -o comm=)
+    nome_processo= $(ps -p $pid -o comm=)
+    echo $(date +%F,%H:%M:%S) >> $nome_processo
 done
