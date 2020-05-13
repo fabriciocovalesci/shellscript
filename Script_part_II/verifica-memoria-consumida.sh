6fbd3f500066ff6e83986a0e77c33f595b19be28
Fabricio <fabcovalesci@gmail.com>
2020-05-13 13:37:37 -0300

    enviando email, se memoria do sistema estiver abaixo de 50%

--- Script_part_II/verifica-memoria-consumida.sh
+++ Script_part_II/verifica-memoria-consumida.sh
@@ -5,5 +5,11 @@ memoria_total=$(free | grep -i mem | awk '{ print $2 }')
 memoria_consumida=$(free | grep -i mem | awk '{ print $3 }')
 relacao_memoria_atual_total=$(bc <<< "scale=2;$memoria_consumida/$memoria_total *100" | awk -F. '{ print $1 }')
 
+if [ $relacao_memoria_atual_total -gt 50 ]
+then
+mail -s "Consumo de memoria acima do limite" fabcovalesci@gmail.com<<del
+O consumo de memoria esta acima do limite que foi espeficicado  atualmente o consumo é de $(free -h | grep -i mem | awk '{ print $3 }')
+del
+fi
 
 echo $relacao_memoria_atual_total
