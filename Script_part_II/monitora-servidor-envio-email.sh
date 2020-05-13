5bf7b868fb156c54f7bc4dbbc1f6ff6be987efbd
Fabricio <fabcovalesci@gmail.com>
2020-05-13 11:07:12 -0300

    Enviando um email, para admin, por shell script, caso servidor pare de funciona

--- Script_part_II/monitora-servidor-envio-email.sh
+++ Script_part_II/monitora-servidor-envio-email.sh
@@ -2,17 +2,17 @@
 
 resposta_http=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost)
 
-if [ $resposta_http -eq 200 ]
+if [  $resposta_http -ne 200 ]
 then
-    echo "Tudo esta ok com servidor"
+# Envia email para o administrador
+mail -s "Problema no servidor" fabcovalesci@gmail.com<<del
+Houve um problema com o servidor, e os usuarios estão sem acesso.
+del
+
 else
     echo "Houve um problema com servidor. Tentando reinicializar"
     #systemctl restart apache2 # Se caso o servidor for apache 
-    python -m http.server 8080 # Servidor subindo com python
     echo "...Servidor reinicializado"
-
+    python -m http.server 8080 # Servidor subindo com python
 fi
 
-
-#sudo crontab -e
-# */2 * * * * /home/fabricio/Documentos/shellscript/Script_part_II/monitaracao-s$
